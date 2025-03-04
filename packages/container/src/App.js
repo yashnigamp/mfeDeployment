f9b6ec5a3cc8461f8afe2efc47593f1fc796178e
yashnigamp <yashnigam.p@gmail.com>
2025-03-03 21:33:21 -0700

    Changes in Header Component

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
-
+import Header from './components/Header'
+import { BrowserRouter } from "react-router-dom";
 export default () => {
   return (
+    <BrowserRouter>
     <div>
-      <h1>Hi there !</h1>
-      <hr />
-      <hr />
+      <Header/>
       <MarketingApp />
     </div>
+    </BrowserRouter>
   );
 };
